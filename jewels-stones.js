6af59cc1b377a7709b3c98d61a1e70d437dc84f6
Jackie Ye <jackieye27@gmail.com>
2021-03-27 18:29:06 -0700

    solved jewels and stones problem

--- jewels-stones.js
+++ jewels-stones.js
@@ -10,8 +10,27 @@
 // Input: jewels = "z", stones = "ZZ"
 // Output: 0
 
+// we are given a string that is case sensitive
+// we want to find out how many of a given letter combo is in the string
 
+// seperate the string so we can check each individual letter
+// then compare it to the given stones we have to see if there are any
 
 let numJewelsInStones = ((j,s) => {
+    // create a count of the number of stones we have
+    let count = 0;
+    // create the 2 lists
+    let jewelsArr = j.split("");
+    let stonesArr = s.split("");
+    // compare each jewele to each stone
+    jewelsArr.forEach(jewel => {
+        stonesArr.forEach(stone => {
+            if (stone === jewel) {
+                count++
+            }
+        })
+    })
+    return count;
+});
 
-})
+numJewelsInStones("aAAbbbb", "aA"); //3
