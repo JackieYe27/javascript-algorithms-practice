e6f3734fe2c2ee00adeb0c1256352f99dcd4c13c
Jackie Ye <jackieye27@gmail.com>
2021-04-06 20:31:30 -0700

    solved two string array problem

--- two-string-array.js
+++ two-string-array.js
@@ -17,7 +17,25 @@
 // Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
 // Output: true
 
+// input 2 arrays with strings inside
+// output is boolean if string inside is same for both
+
+// take both arrays make then into just one string and then split them back up to compare each element
+
 
 let arrayStringsAreEqual = (word1, word2) => {
+    let sWord1 = word1.join("").split("")
+    let sWord2 = word2.join("").split("")
+    let same = false;
 
+    for (i = 0; i < sWord1.length; i++) {
+        if (sWord1[i] === sWord2[i] && sWord1.length === sWord2.length) {
+            same = true;
+        } else {
+            same = false;
+        }
+    }
+    return same;
 }
+
+arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])
