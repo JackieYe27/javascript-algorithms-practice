48d79818901d9eb9343b0e47ecf5ea472df37652
Jackie Ye <jackieye27@gmail.com>
2021-04-09 15:12:13 -0700

    solved number of consistent strings problem

--- numer-of-consistent-strings.js
+++ numer-of-consistent-strings.js
@@ -17,6 +17,32 @@
 // Output: 4
 // Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
 
+// given string and an array or strings
+// output is a number
+
+// we only want to return words in the words array where only the letters from allowed are in
+// even single letters returned are true
+// create a count variable
+// loop through the words array and get each word
+// then check the individual word with what is allowed 
+// and within each word check each letter if it is included in the allowed string
+    // if each letter is included then count it
+
 let countConsistentStrings = (allowed, words) => {
-    
+    let count = 0;
+    words.forEach(word => { // a b
+        let isAllowed = true;
+        for (let i = 0; i < word.length; i++) { // a 
+            let character = word[i];  // word[0] = a
+            if (!allowed.includes(character)) { 
+                isAllowed = false;
+            } 
+        }
+        if (isAllowed) {
+            count++ // +1
+        }
+    });
+    return count;
 }
+
+countConsistentStrings("abc", ["a","b","c","ab","ac","bc","abc"]);
