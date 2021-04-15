e213877f12206c76ec694efe16ca1507b5597bb7
Jackie Ye <jackieye27@gmail.com>
2021-04-15 12:18:57 -0700

    added same arrays squared problem

---
+++ same-array-squared.js
@@ -0,0 +1,9 @@
+/*
+    Write a function called same, which accpest stwo arrays. The function should 
+    return true if every value in the array has it's corresponging value squared in the same array.
+    The frequency of values must be the same as well.
+*/
+
+let same = (arr1, arr2) => {
+    
+}
