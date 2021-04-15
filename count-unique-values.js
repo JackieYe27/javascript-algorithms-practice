a25b5af886396b1bf6696eaea16b7ef2f743bc35
Jackie Ye <jackieye27@gmail.com>
2021-04-15 14:11:06 -0700

    added unique case

--- count-unique-values.js
+++ count-unique-values.js
@@ -15,6 +15,9 @@
 
 let countUniqueValues = (arr) => {
     let i = 0;
+    if (arr.length = 0) {
+        return 0;
+    }
     for (let j = 1; j < arr.length; j++) {
         if (arr[i] !== arr[j]) {
             i++;
