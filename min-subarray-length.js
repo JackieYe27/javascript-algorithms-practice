6c96cf0a53f95223c27c0790d9b792b86b84bcea
Jackie Ye <jackieye27@gmail.com>
2021-04-16 14:47:45 -0700

    added min subarray length problem

---
+++ min-subarray-length.js
@@ -0,0 +1,10 @@
+/*
+    Write a function called minSubArrayLen which accepts two parameters -
+    an array of positive integers and a positive integer. This function should return the minimal length 
+    of a contagious subarray of which the sum is greater than or equal to the integer passed to the function. If there isnt
+    one return 0 instead
+*/
+
+let minSubArrayLen = (array, int) => {
+    
+}
