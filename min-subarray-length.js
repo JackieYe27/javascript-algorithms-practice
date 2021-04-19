8834204346adb4b1437684e743400996e199ca5f
Jackie Ye <jackieye27@gmail.com>
2021-04-18 18:43:03 -0700

    sort of solved minSubArrayLen problem

--- min-subarray-length.js
+++ min-subarray-length.js
@@ -5,6 +5,32 @@
     one return 0 instead
 */
 
-let minSubArrayLen = (array, int) => {
-    
+// input is an array and an integer
+// output is the length of an array where that array is sum is greater than or equal to the integer passed in
+
+// create a window where you will be moving one digit at a time if the windo sum is not equal to sum
+// if current window equals to the sum
+    // we can return the minimum length of the array
+
+let minSubArrayLen = (array, sum) => {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while(start < array.length) {
+        if (total < sum && end < array.length) {
+            total += array[end];
+            end ++;
+        }
+        else if (total >= sum) {
+            minLen = Math.min (minLen, end-start);
+            total -= array[start];
+            start ++;
+        }
+        else {
+            break;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen;
 }
