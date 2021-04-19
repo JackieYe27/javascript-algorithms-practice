224634563d164561739edef42804e9501c8b9c87
Jackie Ye <jackieye27@gmail.com>
2021-04-18 18:45:05 -0700

    added find longest substring problem

---
+++ find-longest-substring.js
@@ -0,0 +1,8 @@
+/*
+    Write a function called findLongestSubstring, which accepts a string and returns the length of a the longest
+    substring with all distinct characters.
+*/
+
+let findLongestSubstring = (string) => {
+    
+}
