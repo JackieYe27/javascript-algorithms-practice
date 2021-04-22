c3dac990cc622e4f34d9044606abb31505f36db9
Jackie Ye <jackieye27@gmail.com>
2021-04-21 18:44:35 -0700

    bubble sort implementation

---
+++ sorting-algos/bubble-sort.js
@@ -0,0 +1,12 @@
+/*
+    Write bubbleSort function. 
+*/
+
+// Start looping from i to end of array towards the beginning
+// start an iner loop with a variable j from the beggining unti i -1
+// if arr[i} > arr[j+1] swap those two values
+// return sorted array
+
+let bubbleSort = (unsortedArr) => {
+    
+}
