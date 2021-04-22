d4167d2fe7ff346ecf75488aa79d74e7eb56bc3d
Jackie Ye <jackieye27@gmail.com>
2021-04-21 19:04:02 -0700

    implemented bubble sort

--- sorting-algos/bubble-sort.js
+++ sorting-algos/bubble-sort.js
@@ -7,6 +7,38 @@
 // if arr[i} > arr[j+1] swap those two values
 // return sorted array
 
-let bubbleSort = (unsortedArr) => {
-    
+// forced dumb way of implementing bubble sort
+let bubbleSort = (arr) => {
+    for (let i =0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+        }
+    }
+    return arr;
 }
+
+// Optimized with no swaps for nearly sorted array
+let bubbleSort = (arr) => {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            } 
+        }
+        if (noSwaps) {
+            break;
+        }
+    }
+    return arr;
+}
+
+bubbleSort([1,2,3,4,4,1,10,7,6,1])
