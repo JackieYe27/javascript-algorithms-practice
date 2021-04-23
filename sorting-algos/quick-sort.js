71ac134a35fd554ccbdd3c573368c6392f9932ea
Jackie Ye <jackieye27@gmail.com>
2021-04-23 14:52:29 -0700

    implemented quick sort

--- sorting-algos/quick-sort.js
+++ sorting-algos/quick-sort.js
@@ -47,3 +47,20 @@ let pivot = (arr, start=0, end=arr.length-1) => {
 }
 
 pivot([4,8,2,1,5,7,6,3])
+
+
+// call pivot helper for array
+// recursively call the helper function on the left side and on the right side
+// base case check sub array has any items in it
+let quickSort = (arr, left = 0, right = arr.length-1) => {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right); // 3
+        // left
+        quickSort(arr, left, right = pivotIndex-1);
+        //right
+        quickSort(arr, pivotIndex+1, right);
+    }
+    return arr;
+}
+
+quickSort([4,8,2,1,5,7,6,3]);
