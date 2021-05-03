2a777565ffc1823fad99ace740aab9c9be7d6c2e
Jackie Ye <jackieye27@gmail.com>
2021-05-03 15:15:47 -0700

    implemeneted queue dequeue and enqueue

--- data-structures/stacks-and-queues.js
+++ data-structures/stacks-and-queues.js
@@ -1,5 +1,5 @@
 /*
-    Stacks:
+    Stacks: Only Adding and Removing
         LIFO: LAST IN FIRST OUT
 */
 
@@ -43,6 +43,44 @@ class Stack {
     }
 }
 
+/*
+    QUEUES: Only Adding and removing
+        FIFO: FIRST IN FIRST OUT
+*/
 
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
-
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.size++;
+        return this.size;
+    }
+    dequeue() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+}
