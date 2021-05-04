171ff5c6e757dfd5650665938bf05597aacb899b
Jackie Ye <jackieye27@gmail.com>
2021-05-04 10:06:47 -0700

    creating a binary search tree

---
+++ data-structures/binary-search-trees.js
@@ -0,0 +1,18 @@
+/*
+    Binary Search Trees: 
+*/
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
+
