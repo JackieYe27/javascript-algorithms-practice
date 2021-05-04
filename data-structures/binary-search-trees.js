5802c452d0317684006819dba73a1ead0af21ed2
Jackie Ye <jackieye27@gmail.com>
2021-05-04 10:35:30 -0700

    implemeneted insert into BST

--- data-structures/binary-search-trees.js
+++ data-structures/binary-search-trees.js
@@ -14,5 +14,34 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(val) {
+        let newNode = new Node(val);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while(true) {
+                if (val === current.val) {
+                    return undefined;
+                }
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }   
+                }
+            }
+        }
+    }
 }
 
