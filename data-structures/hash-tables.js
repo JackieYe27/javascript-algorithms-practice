7ca14e0415d2e04ff323f90a6e48bf6bf971183e
Jackie Ye <jackieye27@gmail.com>
2021-05-07 11:04:44 -0700

    impoved first hash function

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -5,10 +5,24 @@
 // not constant, only hashes strings, little random
 let hash = (key, arrayLen) => {
     let total = 0;
-    for (let char of key) {
+    for (let i = 0; i < key.length; i++) {
+        let char = key[i];
         let value = char.charCodeAt(0) - 96;
         total = (total + value) % arrayLen;
     }
     return total;
 }
 
+// improving the function
+    // adding prime numbers to create better distribution
+
+let hash = (key, arrayLen) => {
+    let total = 0;
+    let prime = 31;
+    for (let i =0; i < key.length; i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * prime + value) % arrayLen;
+    }
+    return total;
+}
