7d4efebc8eeb8bc93bae945a5441e8abb95d1eba
Jackie Ye <jackieye27@gmail.com>
2021-05-09 14:57:34 -0700

    undestaanding graphs

---
+++ data-structures/graphs.js
@@ -0,0 +1,20 @@
+/*
+    Graphs: Collections of Nodes and connections
+    VERTEXT = node
+    EDGE = connection between nodes
+        Good for What?
+        - Social networking: Connections between friends
+        - Maps
+        - the internet: pages that link to each other
+        - Recommendations: people also watched, you might like
+        No Patterns Just relations to each other
+    Types:
+        - A tree where a vertex can only have 2 edges max
+        - Undirected Graphs: no connection to edges. Basically 2 way connections. Can go from A to B and B to A.
+            - FB friend request 2 way relationship.
+        - Directed Graphs: Represented with arrows. One way direction.
+            - Instagram followers graph. Following someone but not getting followed back.
+        - Weighted Graph: when edges have values. Theres info on the connections itself
+            - Mapping distance. The edges in miles are weighted. Need to know value
+*/
+
--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -60,4 +60,30 @@ class HashTable {
         }
         return undefined;
     }
+    key() {
+        if(this.keyMap) {
+            let keys = [];
+            for(let i = 0; i < this.keyMap.length; i++) {
+                if(this.keyMap[i])
+                    for(let j = 0; j < this.keyMap[i].length; j++) {
+                        keys.push(this.keyMap[i][j][0]);
+                    }
+            }
+            return keys;
+        }
+        return undefined;
+    }
+    values() {
+        let valuesArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (valuesArr.includes(this.keyMap[i][j][1])) {
+                        values.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
+    }
 }
