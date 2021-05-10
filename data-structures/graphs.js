ca11d9d157b600feaa2518df473b8f7be20dbd0f
Jackie Ye <jackieye27@gmail.com>
2021-05-10 11:54:44 -0700

    implemeneted a graph with addVertex, addEdge, and removeEdge methods

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -16,5 +16,34 @@
             - Instagram followers graph. Following someone but not getting followed back.
         - Weighted Graph: when edges have values. Theres info on the connections itself
             - Mapping distance. The edges in miles are weighted. Need to know value
+    Adjacency List: Take up less space, faster to iterate over all edges but slower look up on specific edges
+        - REAL WORLD DATA TEND TO BE SPARSE. MANY NODES not so many CONNECTIoNS. We take up less space
+    Adjacency Matrix: More space, slower iteration, but fast lookup for specific edge
 */
 
+// Undirected Graph : adding a vertex
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    // adding nodes aka vertices
+    addVertex(vertex) {
+        if(!this.adjacencyList) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    // adding edges: should accept 2 vertices we can call them v1 and v2
+    // the function should find in the adjacency list the key of v1 and push v2 to the array
+    // then function should find in the adjacency list the key of v2 and push v1 to the array
+    addEdge(v1,v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+    // Removing an edge: what is the actual edge we want to remove
+    // function should reassign the key of v1 to be an array that does not contain v2
+    // function should reassign the ky of v2 to be an array that does not contain v1
+    removeEdge(v1,v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(edge => edge !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(edge => edge !== v1);
+    }
+}  
