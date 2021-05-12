ef11c4f59cca872c0c67f59780fd21f2e07b2174
Jackie Ye <jackieye27@gmail.com>
2021-05-11 17:23:44 -0700

    created better priority queue

--- data-structures/Dijkstra's Algorithm.js
+++ data-structures/Dijkstra's Algorithm.js
@@ -2,6 +2,75 @@
     Shortest Path Algo: Finding the fastest route.
 */
 
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values= [];
+    }
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp()
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        let value = this.values[idx];
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx-1)/2);
+            let parent = this.values[parentIdx];
+            if(value.priority >= parent.priority) break;
+            this.values[parentIdx] = value;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    dequeue() {
+        let min = this.values[0];
+        let end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+    sinkDown() {
+        let idx = 0;
+        let length = this.values.length;
+        let value = this.values[0];
+        while(true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority < value.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild.priorty < value.priority) || 
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = value;
+            idx = swap;
+        }
+    }
+}
+
 // Creating a weighted graph
 class WeightedGraph {
     constructor() {
@@ -16,21 +85,17 @@ class WeightedGraph {
         this.adjacencyList[v1].push({node:v2, weight});
         this.adjacencyList[v2].push({node:v1, weight});
     }
-}
-
-// Basic naive priority queue with the goal of getting the smallest priority
-class PriorityQueue {
-    constructor() {
-        this.values = [];
-    };
-    enqueue(val, priority) {
-        this.values.push({val, priority});
-        this.sort();
-    };
-    dequeue() {
-        return this.values.shift();
-    };
-    sort() {
-        this.values.sort((a,b) => a.priority - b.priority);
-    };
+    dijktras(start, end) {
+        let nodes = new PriorityQueue();
+        let distances = {};
+        let previous = {};
+        for(let key in this.adjacencyList) {
+           if(key === start) {
+               distances[key] = 0;
+           } else {
+                istances[key] = Infinity;
+           }
+       }
+        console.log(distances);
+    }
 }
