e3174259a0c0f562709c631ff5df6ccb795f55ff
Jackie Ye <jackieye27@gmail.com>
2021-05-21 15:24:28 -0700

    max depth of binary tree

--- data-structures/binary-search-trees.js
+++ data-structures/binary-search-trees.js
@@ -149,3 +149,13 @@ class BinarySearchTree {
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////////
+// Tree Problems
+
+var maxDepth = function(root) {
+    if (root === null) return 0;
+    
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    return Math.max(left, right) + 1;
+};
