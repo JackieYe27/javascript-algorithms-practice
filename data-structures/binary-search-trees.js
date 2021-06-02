3d5c380d4caa6ca8bcce335df2d4a55e015b0dd1
Jackie Ye <jackieye27@gmail.com>
2021-06-01 22:07:05 -0700

    add rangeSumBST problem

--- data-structures/binary-search-trees.js
+++ data-structures/binary-search-trees.js
@@ -159,3 +159,9 @@ var maxDepth = function(root) {
     let right = maxDepth(root.right);
     return Math.max(left, right) + 1;
 };
+
+// Given the root node of a binary search tree and two integers low and high, 
+// return the sum of values of all nodes with a value in the inclusive range [low, high].
+var rangeSumBST = function(root, low, high) {
+    
+};
