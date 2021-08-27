bff887cbd6507aae431e0ea88d0c5a4ca69948c9
Jackie Ye <jackieye27@gmail.com>
2021-08-27 16:19:36 -0700

    add and solve removeNthFromBack of list

--- data-structures/singly-linked-lists.js
+++ data-structures/singly-linked-lists.js
@@ -166,3 +166,40 @@ var deleteNode = function(node) {
     node.val = newCurrent.val;
     node.next = nextNode;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ * 
+ 
+Given the head of a linked list, remove the nth node from the end of the list and return its head. 
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+ */
+
+const removeNthFromBack = (head, n) => {
+    let dummy = new ListNode();
+    let fast = dummy;
+    let slow = dummy;
+
+    for (let i = 0; i < n; i++) {
+        fast = fast.next;
+    }
+
+    while(fast.next) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+    return dummy.next;
+}
