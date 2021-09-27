70df67cd1b1e5c63dbda44e6f2f0637851f587b3
Jackie Ye <jackieye27@gmail.com>
2021-09-27 14:45:54 -0700

    oneEditAway problem

--- cracking-coding-interview-problem-sets.js
+++ cracking-coding-interview-problem-sets.js
@@ -152,7 +152,37 @@ const urlify = (str) => {
   Examples: cake - ake, pale - ple
 
 */
+// Check for different kinds of edits
+  // Insert: s1's current char === s2's next char
+  // remove: s1's next char === s2's current char
+  // replace: s1's next char should match s2's next char
 
 const oneEditAway = (s1, s2) => {
-  
+  let edits = 1;
+  let maxLen = Math.max(s1.length, s2.length);
+  let diff = Math.abs(s1.length - s2.length);
+
+  if (diff > edits) {
+    return false;
+  }
+
+  for (let i = 0, j =0; i < maxLen || j < maxLen; i++, j++) {
+    let c1 = s1[i];
+    let c2 = s2[j];
+
+    if (c1 !== c2) {
+      edits--;
+
+      if (edits < 0) {
+        return false;
+      }
+
+      if (c1 === s2[j + 1]) {
+        j++;
+      } else if (s1[i + 1] === c2) {
+        i++;
+      }
+    }
+  }
+  return true;
 }
